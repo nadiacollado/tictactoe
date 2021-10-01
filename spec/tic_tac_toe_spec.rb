95971179a513e54bc21a36a52951d1421d970552
Nadia Collado <collado.nadia@gmail.com>
2021-10-01 13:55:39 -0400

    added test that checks if game's instructions are returned

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,7 +3,13 @@ require "tic_tac_toe"
 describe TicTacToe do
     describe "welcome" do
         it "returns welcome string" do
-            expect(TicTacToe.new.start_app()).to eq("Welcome to Tic-Tac-Toe!")
+            expect(TicTacToe.new.welcome()).to eq("Welcome to Tic-Tac-Toe!")
+        end
+    end
+
+    describe "game_instructions" do
+        it "returns game instructions string" do
+            expect(TicTacToe.new.game_instructions()).to eq("These are the game's instructions!")
         end
     end
     
