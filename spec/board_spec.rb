fc93695ddcc46000e2ec4f76705eb8565ffb4edf
Nadia Collado <collado.nadia@gmail.com>
2021-10-06 16:51:29 -0400

    changed Board test from equality to output matcher

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,23 +2,8 @@ require "board"
 
 describe Board do
     describe "display_board" do
-        before do
-            @board = Board.new
-            @display = @board.display_board
-        end
-
-        it "returns a string" do
-            expect(@display).to be_kind_of(String)
-        end
-
         it "returns game board diplay" do
-            expect{@board.display_board()}.to output("
- 1 | 2 | 3              
------------                 
- 4 | 5 | 6              
------------                
- 7 | 8 | 9"
-).to_stdout
+            expect{Board.new.display_board()}.to output("\n 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9\n        \n").to_stdout
         end
     end
 end
