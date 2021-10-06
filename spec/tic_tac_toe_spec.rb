0f2d6c9b5b8f5c47bd4d210542f6e70274749054
Nadia Collado <collado.nadia@gmail.com>
2021-10-06 16:44:11 -0400

    bug: test for welcome method on TTT class returns NoMethodError

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,13 +2,19 @@ require "tic_tac_toe"
 
 describe TicTacToe do
     describe "welcome" do
-        it "returns welcome string" do
-            expect {TicTacToe.new.welcome()}.to output("\nWelcome to Tic-Tac-Toe!\n").to_stdout
+        before do
+            @app = TicTacToe.new
+        end
+        it "prints string" do
+            expect (@app.welcome()).to eq("\nWelcome to Tic-Tac-Toe!\n")
+        end
+        it "prints welcome message" do
+            expect {@app.welcome()}.to output("\nWelcome to Tic-Tac-Toe!\n").to_stdout
         end
     end
 
     describe "game_instructions" do
-        it "returns game instructions string" do
+        it "prints game instructions string" do
             expect{TicTacToe.new.game_instructions()}.to output(
                 "\nEach square in the board corresponds to a number 1-9.\nMake your move by entering the number in the square of your choice.\n").to_stdout
         end
