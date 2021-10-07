6c2a67f641d5b4afc41848d20b728f55f8622755
Nadia Collado <collado.nadia@gmail.com>
2021-10-07 15:10:09 -0400

    bug: valid_move's square_taken test fails

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,32 +1,32 @@
 require "board"
 
 describe Board do
+    before do
+        @board = Board.new
+    end
+    
     describe "initialize" do
-        it "creates an array of string numbers 1-9" do
+        it "should creates an array of string numbers 1-9" do
             expect(Board.new.squares).to eq(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
         end
     end
      
     describe "display_board" do
-        it "returns game board diplay" do
-            expect{Board.new.display_board()}.to output("\n 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9\n        \n").to_stdout
+        it "should return game board diplay" do
+            expect{@board.display_board()}.to output("\n 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9\n        \n").to_stdout
         end
     end
 
     describe "valid_move?" do
-        it "returns true if player's move falls within the allotted range 1-9" do
-            move = 4
-            outcome = Board.new.valid_move?(move)
-            expect(outcome).to eq(true)
-        end
-        it "returns false if player's move does not fall within the alloted range 1-9" do
+        it "should return false if player's move falls outside of 1-9 range" do
             move = 25
-            outcome = Board.new.valid_move?(move)
+            outcome = @board.valid_move?(move)
             expect(outcome).to eq(false)
         end
-        #it "returns truthy or falsey value" do
-        #end
-        #it "calls square_taken? once"
-        #end
+
+        it "should call square_taken? once if player's move falls within 1-9 range" do
+            move = 4
+            expect(@board.valid_move?(4)).to receive(:square_taken?).once
+        end
     end
 end
