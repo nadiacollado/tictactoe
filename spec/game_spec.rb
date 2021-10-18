2b759a9dd936d4aedd32341d39876d0998e6a530
Nadia Collado <collado.nadia@gmail.com>
2021-10-18 18:38:13 -0400

    tests that turn method does not mark a square with a player's invalid move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,10 +7,7 @@ describe Game do
 
     describe "player" do
         it "returns current player" do
-            squares = [
-                "1", "X", "3", 
-                "O", "O", "X", 
-                "7", "8", "9" ]
+            squares = %w[1 X 3 O O X 7 8 9]
             board.instance_variable_set(:@squares, squares)
             game.instance_variable_set(:@board, board)
             expect(game.player.symbol).to eq("X")
@@ -19,31 +16,32 @@ describe Game do
 
     describe "turn" do
         it "marks the square on the board with a player's valid move" do
+            expect(game.board.squares).to eq(%w[1 2 3 4 5 6 7 8 9])
             allow(game.player).to receive(:get_move).and_return(3)
             game.turn
-            expect(game.board.squares).to eq([
-                "1", "2", "X", 
-                "4", "5", "6", 
-                "7", "8", "9" ])
+            expect(game.board.squares).to eq(%w[1 2 X 4 5 6 7 8 9])
+        end
+
+        it "does not mark the square on the board if a player's move is invalid" do
+            squares = %w[1 2 3 4 X 6 7 8 9]
+            board.instance_variable_set(:@squares, squares)
+            game.instance_variable_set(:@board, board)
+            allow(game.player).to receive(:get_move).and_return(5)
+            game.turn
+            expect(game.board.squares).to eq(%w[1 2 3 4 X 6 7 8 9])
         end
     end
 
     describe "won?" do
         it "returns winner of game if game has been won" do
-            squares = [
-                "X", "X", "X", 
-                "O", "O", "X", 
-                "O", "X", "O" ]
+            squares = %w[X X X O O X O X O]
             board.instance_variable_set(:@squares, squares)
             game.instance_variable_set(:@board, board)
             expect(game.won?).to eq("X")
         end
 
         it "returns false if game has not been won" do
-            squares = [
-                "X", "2", "X", 
-                "O", "5", "X", 
-                "7", "X", "O" ]
+            squares = %w[X 2 X O 5 X 7 X O]
             board.instance_variable_set(:@squares, squares)
             game.instance_variable_set(:@board, board)
             expect(game.won?).to eq(false)
@@ -52,10 +50,7 @@ describe Game do
 
     describe "draw?" do
         it "returns true if the board is full and there is no winner" do
-            squares = [
-                "X", "O", "X", 
-                "O", "O", "X", 
-                "O", "X", "O" ]
+            squares = %w[X O X O O X O X O]
             board.instance_variable_set(:@squares, squares)
             game.instance_variable_set(:@board, board)
             expect(game.draw?).to eq(true)
