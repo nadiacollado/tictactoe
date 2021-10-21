c506f7543deeaf206d105065b152823fa693b016
Nadia Collado <collado.nadia@gmail.com>
2021-10-21 10:39:27 -0400

    removes new Rules instance from each test in rules_spec

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -8,13 +8,11 @@ describe Rules do
     describe "won?" do
         it "returns winner of game if game has been won" do
             board = Board.new(%w[X X X O O X O X O])
-            rules = Rules.new
             expect(rules.won?(board)).to eq("X")
         end
 
         it "returns false if game has not been won" do
             board = Board.new(%w[X 2 X O 5 X 7 X O])
-            rules = Rules.new
             expect(rules.won?(board)).to eq(false)
         end
     end
@@ -22,13 +20,11 @@ describe Rules do
     describe "winner" do
         it "returns winner of game if game has been won" do
             board = Board.new(%w[X O X O O O O X X])
-            rules = Rules.new
             expect(rules.winner(board)).to eq("O")
         end
 
         it "returns false if game has not been won" do
             board = Board.new(%w[X 2 X O 5 X 7 X O])
-            rules = Rules.new
             expect(rules.winner(board)).to eq(false)
         end
     end
@@ -36,7 +32,6 @@ describe Rules do
     describe "draw?" do
         it "returns true if the board is full and there is no winner" do
             board = Board.new(%w[X O X O O X O X O])
-            rules = Rules.new
             expect(rules.draw?(board)).to eq(true)
         end
     end
@@ -44,7 +39,6 @@ describe Rules do
     describe "game_over?" do
         it "returns true if the board is full and there is no winner" do
             board = Board.new(%w[X O X O O X O X O])
-            rules = Rules.new
             expect(rules.game_over?(board)).to eq(true)
         end
     end
