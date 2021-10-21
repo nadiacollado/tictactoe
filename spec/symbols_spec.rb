a5bf8f0f4dff3c82c5f1955ded5583ce1016de1d
Nadia Collado <collado.nadia@gmail.com>
2021-10-21 11:15:14 -0400

    adds tests for player1_marker method

--- spec/symbols_spec.rb
+++ spec/symbols_spec.rb
@@ -3,4 +3,10 @@ require 'symbols'
 describe Symbols do
     let (:symbols) { Symbols.new }
 
+    describe "player1_marker" do
+        it "returns the first player's symbol" do
+            expect(symbols.player1_marker).to eq ("X")
+        end
+    end
+
 end
