98ff880b68fc0e0076de14fece2c9a11c9ea522d
Nadia Collado <collado.nadia@gmail.com>
2021-10-28 18:09:06 -0400

    adds move_generator test to computer_spec

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -5,9 +5,15 @@ describe Computer do
     let (:computer_player) { Computer.new(1, marker.p1_marker, "computer")}
 
     describe "initialize" do
-        fit "creates a computer player with given arguments" do
+        it "creates a computer player with given arguments" do
             expect(computer_player.id).to eq(1)
             expect(computer_player.marker).to eq(marker.p1_marker)
+        end
+    end
+
+    describe "move_generator" do
+        it "randomly selects a number 1-9" do
+            expect(computer_player.move_generator).to be_between(1, 9)
         end
     end
 end
