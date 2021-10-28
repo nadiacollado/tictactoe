6842020c00905e6f9f5bf1f1ba6de08ffc42a323
Nadia Collado <collado.nadia@gmail.com>
2021-10-28 17:53:20 -0400

    tests that a Computer instance is created

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,4 +1,13 @@
 require 'computer'
 
 describe Computer do
+    let (:marker) { Marker.new("X", "O") }
+    let (:computer_player) { Computer.new(1, marker.p1_marker, "computer")}
+
+    describe "initialize" do
+        fit "creates a computer player with given arguments" do
+            expect(computer_player.id).to eq(1)
+            expect(computer_player.marker).to eq(marker.p1_marker)
+        end
+    end
 end
