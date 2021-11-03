104f7357ce70d306ce370136254d21ea418ae058
Nadia Collado <collado.nadia@gmail.com>
2021-11-03 12:12:29 -0400

    adds initialize test to player_spec

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,4 +1,6 @@
 class Player
+    attr_reader :id, :marker, :type
+    
     def initialize(id, marker, type)
         @id = id
         @marker = marker
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,13 +2,13 @@ require 'player'
 
 describe Player do
     let (:marker) { Marker.new("A", "B")}
-    let (:player) { Player.new(marker) }
+    let (:player) { Player.new(1, marker.p1_marker, HUMAN_PLAYER) }
 
-    # describe "set_players" do
-    #     it "creates human player" do
-    #         player.set_players(marker.p1_marker, "H")
-    #         expect(player.player1.id).to eq(1)
-    #         expect(player.player1.marker).to eq("A")
-    #     end
-    # end
+    describe "initialize" do
+        it "creates a player with the correct marker and id" do
+            expect(player.marker).to eq("A")
+            expect(player.id).to eq(1)
+            expect(player.type).to eq("H")
+        end
+    end
 end
