d2fa6a9828afd21b2ff45d6b0980fa403c0b20e9
Nadia Collado <collado.nadia@gmail.com>
2021-11-04 12:37:40 -0400

    adds Player class to human_spec and computer_spec

--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -4,19 +4,20 @@ require 'human'
 describe Human do
     let (:board) { Board.new }
     let (:marker) { Marker.new("X", "O") }
-    let (:test_player) { Human.new(1, marker.p1_marker, HUMAN_PLAYER)}
+    let (:player) { Player.new(HUMAN_PLAYER) }
+    let (:human_player) { Human.new(1, marker.p1_marker, HUMAN_PLAYER)}
 
     describe "initialize" do
         it "creates a new human player with given arguments" do
-            expect(test_player.id).to eq(1)
-            expect(test_player.marker).to eq(marker.p1_marker)
+            expect(human_player.id).to eq(1)
+            expect(human_player.marker).to eq(marker.p1_marker)
         end
     end
 
     describe "get_move" do
         it "returns the current player's move via the gets method" do
-            allow(test_player).to receive(:gets).and_return("1")
-            expect(test_player.get_move(board)).to eq(1)
+            allow(human_player).to receive(:gets).and_return("1")
+            expect(human_player.get_move(board)).to eq(1)
         end
     end
 end
